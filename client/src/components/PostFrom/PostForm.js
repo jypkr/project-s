d3488394cb06896e0142e625eb5a28f59bcc91d3
Gilberto Resendiz <gresendi@uci.edu>
2021-12-08 14:44:03 -0800

    fixed bug where like and dislike where getting overwriten

--- client/src/components/PostFrom/PostForm.js
+++ client/src/components/PostFrom/PostForm.js
@@ -128,8 +128,8 @@ const PostForm = () => {
   }
 
   const handleLike = async (_id) => {
-    //Need to get UserId
-    let userID = localStorage.getItem('userId')
+    
+    let userID = getUser()
     let post = {
       _id: _id,
       title: 'title',
@@ -143,11 +143,13 @@ const PostForm = () => {
         post.title = element.title
         post.body = element.body
         post.image = element.image
+        post.dislikedBy = element.dislikedBy
         if (element.likedBy && element.likedBy.length > 0) {
           console.log('length >0')
+
           let duplicate = element.likedBy.filter(temp => temp === userID)
           console.log(duplicate)
-          if (duplicate) {
+          if (duplicate!=0) {
             console.log('already in')
             let array = []
             element.likedBy.forEach(temp => {
@@ -167,7 +169,15 @@ const PostForm = () => {
         let flag = post.dislikedBy.length
         console.log(flag)
         if (flag != 0) {
-          post.dislikedBy = element.disliked.filter(element => element !== userID)
+          let array = []
+          element.dislikedBy.forEach(temp => {
+            if (temp !== userID) {
+              array.push(temp)
+            }
+          });
+
+
+          post.dislikedBy = array
         }
 
       }
@@ -191,8 +201,7 @@ const PostForm = () => {
 
   }
   const handleDislike = async (_id) => {
-    //Need to get UserId
-    let userID = localStorage.getItem('userId')
+    let userID = getUser()
     let post = {
       _id: _id,
       title: 'title',
@@ -206,18 +215,13 @@ const PostForm = () => {
         post.title = element.title
         post.body = element.body
         post.image = element.image
-        //filter out userId from element.likedBy
-
-        let flag = post.likedBy.length
-        console.log(flag)
-        if (flag != 0) {
-          post.likedBy = element.likedBy.filter(element => element !== userID)
-        }
-
+        post.likedBy = element.likedBy
         if (element.dislikedBy && element.dislikedBy.length > 0) {
+          console.log('length >0')
+
           let duplicate = element.dislikedBy.filter(temp => temp === userID)
           console.log(duplicate)
-          if (duplicate) {
+          if (duplicate != 0) {
             console.log('already in')
             let array = []
             element.dislikedBy.forEach(temp => {
@@ -227,10 +231,26 @@ const PostForm = () => {
             });
 
             post.dislikedBy = array
+          } else {
+            post.dislikedBy = [...element.dislikedBy, userID]
           }
 
         } else {
           post.dislikedBy = [userID]
+        }
+
+        let flag = post.likedBy.length
+        console.log(flag)
+        if (flag != 0) {
+          let array = []
+          element.likedBy.forEach(temp => {
+            if (temp !== userID) {
+              array.push(temp)
+            }
+          });
+
+
+          post.likedBy = array
         }
 
       }
@@ -247,7 +267,9 @@ const PostForm = () => {
     }
     catch (err) {
       console.error(err)
+
     }
+
   }
 
   return (
