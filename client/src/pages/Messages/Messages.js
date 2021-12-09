4318f5a12f6b40ebeae097b22ff2bfa3d4ccf7b6
Gilberto Resendiz <gresendi@uci.edu>
2021-12-09 14:34:04 -0800

    created Messages Page

---
+++ client/src/pages/Messages/Messages.js
@@ -0,0 +1,67 @@
+import Navbar from '../../components/NavBar/Navbar'
+// import { useState, useEffect } from 'react'
+// import { useQuery, useMutation } from '@apollo/client'
+// import { useStoreContext } from '../../utils/GlobalState.js'
+// import AuthService from '../../utils/auth.js'
+import Box from '@mui/material/Box';
+
+
+
+
+
+const Messages = () =>{
+
+
+
+
+  return (
+
+
+
+    <>
+      <Box display="flex" flexDirection="row" >
+        <Box flexGrow={0}>
+          <Navbar></Navbar>
+        </Box>
+        <Box display="flex" flexDirection="row" flexGrow={6}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            marginLeft='1rem'
+
+            flexGrow={1}
+          >
+
+
+
+            <div className='pgContent'>
+
+              <h1>Messages Page</h1>
+             
+            </div>
+
+
+
+          </Box>
+        </Box>
+        <Box display="flex" flexDirection="row" flexGrow={1}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            marginLeft='1rem'
+
+
+            flexGrow={1}
+          >
+            <h1>Side Bar</h1>
+          </Box>
+        </Box>
+
+      </Box>
+
+    </>
+  )
+}
+
+
+export default Messages
